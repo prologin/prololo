af7527a0b547553b0634c55b7e830eef427da0ef
Antoine Martin <antoine.martin@prologin.org>
2021-09-15 18:04:43 +0200

    bot: move each event to own function

--- src/bot/github.rs
+++ src/bot/github.rs
@@ -1,32 +1,33 @@
 use matrix_sdk::ruma::events::{room::message::MessageEventContent, AnyMessageEventContent};
 
-use crate::webhooks::{github::RefType, GitHubEvent};
+use crate::webhooks::{
+    github::{CreateEvent, RefType},
+    GitHubEvent,
+};
 
 const SEPARATOR: &str = "⋅";
 
-pub fn handle_github_event(
-    event: GitHubEvent,
-) -> anyhow::Result<Option<AnyMessageEventContent>> {
+pub fn handle_github_event(event: GitHubEvent) -> anyhow::Result<Option<AnyMessageEventContent>> {
     let message = match event {
-        GitHubEvent::Create(event) => match event.ref_type {
-            RefType::Branch => return Ok(None), // new branches are handled by the Push event
-            RefType::Tag => {
-                format!(
-                    "[{}] {} created tag {} {} {}",
-                    event.repository.name,
-                    event.sender.login,
-                    event.r#ref,
-                    SEPARATOR,
-                    event.repository.ref_url(&event.r#ref)
-                )
-            }
-        },
+        GitHubEvent::Create(event) => handle_create(event),
         GitHubEvent::Issues => todo!(),
         GitHubEvent::IssueComment => todo!(),
         GitHubEvent::Push => todo!(),
     };
 
-    let message = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(message));
+    Ok(message.map(|m| AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(m))))
+}
 
-    Ok(Some(message))
+fn handle_create(event: CreateEvent) -> Option<String> {
+    match event.ref_type {
+        RefType::Branch => None,
+        RefType::Tag => Some(format!(
+            "[{}] {} created tag {} {} {}",
+            event.repository.name,
+            event.sender.login,
+            event.r#ref,
+            SEPARATOR,
+            event.repository.ref_url(&event.r#ref)
+        )),
+    }
 }
