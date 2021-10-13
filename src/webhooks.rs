93d1132938de2c31da9635d5b2c49857980df7fc
Antoine Martin <antoine.martin@prologin.org>
2021-10-13 19:10:25 +0200

    webhooks: implement prolosite auth

--- src/webhooks.rs
+++ src/webhooks.rs
@@ -3,6 +3,8 @@ use tokio::sync::mpsc::UnboundedSender;
 pub mod github;
 pub use github::{github_webhook, GitHubEvent};
 
+pub mod prolosite;
+
 pub struct EventSender(pub UnboundedSender<Event>);
 
 #[derive(Debug)]
---
+++ src/webhooks/prolosite.rs
@@ -0,0 +1,70 @@
+use anyhow::anyhow;
+use rocket::{
+    http::Status,
+    request::{FromRequest, Outcome},
+    serde::json::Json,
+    State,
+};
+use serde::Deserialize;
+use tracing::trace;
+
+pub struct ProlositeSecret(pub String);
+
+const AUTHORIZATION: &str = "Authorization";
+
+pub(crate) struct AuthorizationHeader<'r>(&'r str);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthorizationHeader<'r> {
+    type Error = anyhow::Error;
+
+    async fn from_request(request: &'r rocket::Request<'_>) -> Outcome<Self, Self::Error> {
+        let authorization = request.headers().get(AUTHORIZATION).collect::<Vec<_>>();
+        if authorization.len() != 1 {
+            trace!("couldn't locate {} header", AUTHORIZATION);
+            return Outcome::Failure((
+                Status::BadRequest,
+                anyhow!("request needs an authorization header"),
+            ));
+        }
+        let authorization = authorization[0];
+        let auth_secret = &request.guard::<&State<ProlositeSecret>>().await.unwrap().0;
+
+        if authorization != auth_secret {
+            trace!("secret validation failed, stopping here...");
+            return Outcome::Failure((Status::BadRequest, anyhow!("secret doesn't match")));
+        }
+
+        trace!("validated Prolosite request");
+        Outcome::Success(AuthorizationHeader(authorization))
+    }
+}
+
+#[rocket::post("/api/webhooks/prolosite/django", format = "json", data = "<payload>")]
+pub(crate) fn django(_token: AuthorizationHeader, payload: Json<DjangoPayload>) {}
+
+#[rocket::post("/api/webhooks/prolosite/forum", format = "json", data = "<payload>")]
+pub(crate) fn forum(_token: AuthorizationHeader, payload: Json<ForumPayload>) {}
+
+#[rocket::post(
+    "/api/webhooks/prolosite/new-school",
+    format = "json",
+    data = "<payload>"
+)]
+pub(crate) fn new_school(_token: AuthorizationHeader, payload: Json<NewSchoolPayload>) {}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct DjangoPayload {
+    request: Request,
+}
+
+#[derive(Debug, Deserialize)]
+struct Request {
+    user: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ForumPayload {}
+
+#[derive(Debug, Deserialize)]
+pub struct NewSchoolPayload {}
